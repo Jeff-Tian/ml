26e02f3c2947305c9c38f6ed8121b9eec8bc9ca7
Jeff Tian <jeff.tian@outlook.com>
2024-11-18 09:10:45 +0800

    feat: add post list to the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,128 +1,162 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import {graphql, Link} from "gatsby"
+import {StaticImage} from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
 const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-  },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
+    {
+        text: "Tutorial",
+        url: "https://www.gatsbyjs.com/docs/tutorial",
+        description:
+            "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+    },
+    {
+        text: "Examples",
+        url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
+        description:
+            "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
+    },
+    {
+        text: "Plugin Library",
+        url: "https://www.gatsbyjs.com/plugins",
+        description:
+            "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+    },
+    {
+        text: "Build and Host",
+        url: "https://www.gatsbyjs.com/cloud",
+        description:
+            "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+    },
 ]
 
 const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-  { text: "Server Side Rendering", url: "using-ssr" },
-  { text: "Deferred Static Generation", url: "using-dsg" },
+    {
+        text: "Page 2",
+        url: "page-2",
+        badge: false,
+        description:
+            "A simple example of linking to another page within a Gatsby site",
+    },
+    {text: "TypeScript", url: "using-typescript"},
+    {text: "Server Side Rendering", url: "using-ssr"},
+    {text: "Deferred Static Generation", url: "using-dsg"},
 ]
 
 const moreLinks = [
-  { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-  {
-    text: "Documentation",
-    url: "https://gatsbyjs.com/docs/",
-  },
-  {
-    text: "Starters",
-    url: "https://gatsbyjs.com/starters/",
-  },
-  {
-    text: "Showcase",
-    url: "https://gatsbyjs.com/showcase/",
-  },
-  {
-    text: "Contributing",
-    url: "https://www.gatsbyjs.com/contributing/",
-  },
-  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
+    {text: "Join us on Discord", url: "https://gatsby.dev/discord"},
+    {
+        text: "Documentation",
+        url: "https://gatsbyjs.com/docs/",
+    },
+    {
+        text: "Starters",
+        url: "https://gatsbyjs.com/starters/",
+    },
+    {
+        text: "Showcase",
+        url: "https://gatsbyjs.com/showcase/",
+    },
+    {
+        text: "Contributing",
+        url: "https://www.gatsbyjs.com/contributing/",
+    },
+    {text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues"},
 ]
 
 const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
-const IndexPage = () => (
-  <Layout>
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
+const IndexPage = ({data}) => (
+    <Layout>
+        <div className={styles.textCenter}>
+            <StaticImage
+                src="../images/example.png"
+                loading="eager"
+                width={64}
+                quality={95}
+                formats={["auto", "webp", "avif"]}
+                alt=""
+                style={{marginBottom: `var(--space-3)`}}
+            />
+            <h1>
+                Welcome to <b>Gatsby!</b>
+            </h1>
+            <h2>Hello</h2>
+            <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+            {data.allMarkdownRemark.edges.map(({node}) => (
+                <div key={node.id}>
+                    <h3>
+                        {node.excerpt}{" "}
+                    </h3>
+                    <p>{node.excerpt}</p>
+                </div>
+            ))}
+            <p className={styles.intro}>
+                <b>Example pages:</b>{" "}
+                {samplePageLinks.map((link, i) => (
+                    <React.Fragment key={link.url}>
+                        <Link to={link.url}>{link.text}</Link>
+                        {i !== samplePageLinks.length - 1 && <> · </>}
+                    </React.Fragment>
+                ))}
+                <br/>
+                Edit <code>src/pages/index.js</code> to update this page.
+            </p>
+        </div>
+        <ul className={styles.list}>
+            {links.map(link => (
+                <li key={link.url} className={styles.listItem}>
+                    <a
+                        className={styles.listItemLink}
+                        href={`${link.url}${utmParameters}`}
+                    >
+                        {link.text} ↗
+                    </a>
+                    <p className={styles.listItemDescription}>{link.description}</p>
+                </li>
+            ))}
+        </ul>
+        {moreLinks.map((link, i) => (
+            <React.Fragment key={link.url}>
+                <a href={`${link.url}${utmParameters}`}>{link.text}</a>
+                {i !== moreLinks.length - 1 && <> · </>}
+            </React.Fragment>
         ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
-    </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
-  </Layout>
+    </Layout>
 )
 
+export const query = graphql`
+    query {
+        allMarkdownRemark {
+            totalCount
+            edges {
+                node {
+                    wordCount {
+                        paragraphs
+                        sentences
+                        words
+                    }
+                    frontmatter {
+                        title
+                    }
+                    excerpt
+                    children {
+                        id
+                    }
+                    html
+                }
+            }
+        }
+    }
+`
 /**
  * Head export to define metadata for the page
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Home"/>
 
 export default IndexPage
