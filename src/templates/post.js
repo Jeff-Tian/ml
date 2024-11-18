a94a052ee91f63e263b3f27e375799cb0d25414e
Jeff Tian <3367820+Jeff-Tian@users.noreply.github.com>
2024-11-18 11:49:53 +0000

    feat: render post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,26 @@
 import React from "react"
+import { graphql } from "gatsby"
+
 import Layout from "../components/layout"
 
-export default () => {
+const Post = ({ data }) => {
+    const post = data.markdownRemark
+
     return (
         <Layout>
-            <div>Hello blog post</div>
+            <div>
+                <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            </div>
         </Layout>
     )
 }
+
+export default Post
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+    }
+  }
+`
