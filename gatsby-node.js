e05b9462b68109fd70a7b05a6ba6b0b5b4629f85
Jeff Tian <3367820+Jeff-Tian@users.noreply.github.com>
2024-11-18 11:45:25 +0000

    feat: add post page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,10 +4,13 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
+
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
@@ -15,4 +18,48 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(JSON.stringify(result, null, 4))
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  console.log(node.internal.type)
+
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `src/posts` })
+    console.log(slug);
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
 }
