4e97c95d6b14d9063b16a90c2c6f37d4ee9c7f9b
Jeff Tian <jeff.tian@outlook.com>
2025-04-13 19:58:18 +0800

    feat: add gatsby-remark-images plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
             options: {
                 plugins: [
                     {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 800,
+                            linkImagesToOriginal: true,
+                            showCaptions: [`title`],
+                            wrapperStyle: `margin: 0 auto;`,
+                        }
+                    },
+                    {
                         resolve: `gatsby-remark-katex`,
                         options: {
                             // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
