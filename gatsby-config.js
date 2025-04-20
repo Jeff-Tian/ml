9691c4a84039c20dee897617b1eb7ef7bb6fd584
Jeff Tian <jeff.tian@outlook.com>
2025-04-20 17:27:50 +0800

    feat: 4.11

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,10 @@ module.exports = {
                         resolve: `gatsby-remark-katex`,
                         options: {
                             // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-                            strict: `ignore`
+                            strict: `ignore`,
+                            macros: {
+                                "\\title": "\\textbf{\\Large #1}",
+                            }
                         }
                     },
                     {
