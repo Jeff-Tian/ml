4232ede4658044225b719aabd486f93da2ef2776
Jeff Tian <jeff.tian@outlook.com>
2025-04-26 15:54:33 +0800

    feat: 4.11 文件下载到本地

--- chinese.bat
+++ chinese.bat
@@ -1,2 +1,12 @@
+@echo off
+REM 设置CMD代码页为UTF-8
 chcp 65001
-set LANG=zh_CN.UTF-8
+REM 设置Git配置
+git config --global core.quotepath false
+git config --global gui.encoding utf-8
+git config --global i18n.commit.encoding utf-8
+git config --global i18n.logoutputencoding utf-8
+git config --global svn.pathnameencoding utf-8
+REM 输出当前设置
+echo Git中文显示配置已完成!
+
---
+++ chinese.ps1
@@ -0,0 +1,2 @@
+$OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
