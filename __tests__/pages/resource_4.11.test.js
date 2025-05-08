896f90e232daadca1acbecbf3407a86a0dc96527
Jeff Tian <jeff.tian@outlook.com>
2025-05-08 18:33:28 +0800

    test: test 2 files

--- __tests__/pages/resource_4.11.test.js
+++ __tests__/pages/resource_4.11.test.js
@@ -1,5 +1,6 @@
 import {render} from '@testing-library/react';
 import {Tree} from "../../src/pages/resource_4.11";
+import {expect} from "playwright/test";
 
 describe('tests for resource 4.11', () => {
     describe('tree', () => {
@@ -49,6 +50,73 @@ describe('tests for resource 4.11', () => {
             expect(result.props.children).toBeDefined();
             expect(result.props.children.length).toBe(1);
             expect(container).toMatchSnapshot();
+        })
+
+        it('should return 2 elements when 2 files', async () => {
+            // arrange
+            const data = {
+                allFile: {
+                    nodes: [
+                        {
+                            name: 'test.txt',
+                            relativePath: 'test.txt',
+                            publicURL: '/test.txt',
+                        },
+                        {
+                            name: 'test2.txt',
+                            relativePath: 'test2.txt',
+                            publicURL: '/test2.txt',
+                        },
+                    ],
+                },
+            };
+
+            // act
+            const result = await Tree({data});
+
+            // assert
+            expect(result).toBeDefined();
+            expect(result.props.children).toBeDefined();
+            expect(result.props.children).toHaveLength(2);
+            expect(result.props.children[0].props.children).toBeDefined();
+
+            const fileDiv = result.props.children[0];
+            expect(fileDiv.type).toBe('div')
+
+            const h2 = fileDiv.props.children[0];
+            expect(h2.type).toBe('h2');
+
+            const ul = fileDiv.props.children[1];
+            expect(ul.type).toBe('ul');
+
+            const li1 = ul.props.children[0];
+            expect(li1.type).toBe('li');
+
+            const a1 = li1.props.children;
+            expect(a1.type).toBe('a');
+            expect(a1.props.href).toBe('/test.txt');
+            expect(a1.props.children).toBe('test.txt');
+            expect(a1.props.target).toBe('_blank');
+            expect(a1.props.rel).toBe('noopener noreferrer');
+
+            const fileDiv2 = result.props.children[1];
+            expect(fileDiv2.type).toBe('div')
+
+            const h22 = fileDiv2.props.children[0];
+            expect(h22.type).toBe('h2');
+
+            const ul2 = fileDiv2.props.children[1];
+            expect(ul2.type).toBe('ul');
+            expect(ul2.props.children).toHaveLength(1);
+
+            const li2 = ul2.props.children[0];
+            expect(li2.type).toBe('li');
+            const a2 = li2.props.children;
+            expect(a2.type).toBe('a');
+            expect(a2.props.href).toBe('/test2.txt');
+            expect(a2.props.children).toBe('test2.txt');
+            expect(a2.props.target).toBe('_blank');
+            expect(a2.props.rel).toBe('noopener noreferrer');
         })
     })
 });
