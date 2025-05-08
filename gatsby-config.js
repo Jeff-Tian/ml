4accbc10c55cc9d407cacab7d242e04b486523eb
Jeff Tian <jeff.tian@outlook.com>
2025-05-08 19:55:41 +0800

    feat: 4.11 的第 5 题题目

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,98 +5,99 @@
  */
 
 /**
- * @type {import('gatsby').GatsbyConfig}
+ * @type {import("gatsby").GatsbyConfig}
  */
 module.exports = {
-    siteMetadata: {
-        title: `Tom M. Mitchell 著《机器学习》之作业练习`,
-        description: `自学《机器学习》之作业练习`,
-        author: `@zizhujy`,
-        siteUrl: `https://ml.jiwai.win/`,
+  siteMetadata: {
+    title: `Tom M. Mitchell 著《机器学习》之作业练习`,
+    description: `自学《机器学习》之作业练习`,
+    author: `@zizhujy`,
+    siteUrl: `https://ml.jiwai.win/`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`
+      }
     },
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                name: `posts`,
-                path: `${__dirname}/src/posts`,
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 800,
-                            linkImagesToOriginal: true,
-                            showCaptions: [`title`],
-                            wrapperStyle: `margin: 0 auto;`,
-                        }
-                    },
-                    {
-                        resolve: `gatsby-remark-katex`,
-                        options: {
-                            // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-                            strict: `ignore`,
-                            macros: {
-                                "\\title": "\\textbf{\\Large #1}",
-                            }
-                        }
-                    },
-                    {
-                        resolve: `gatsby-remark-mermaid`,
-                        options: /** @type {import('gatsby-remark-mermaid').Options} */ ({
-                            mermaidConfig: {
-                                theme: 'neutral',
-                                themeCSS: '.node rect { fill: #fff; }'
-                            },
-                        })
-                    }
-                ],
+              maxWidth: 800,
+              linkImagesToOriginal: true,
+              showCaptions: [`title`],
+              wrapperStyle: `margin: 0 auto;`
             }
-        },
-        `gatsby-plugin-image`,
-        {
-            resolve: `gatsby-source-filesystem`,
+          },
+          {
+            resolve: `gatsby-remark-katex`,
             options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                // This will impact how browsers show your PWA/website
-                // https://css-tricks.com/meta-theme-color-and-trickery/
-                // theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
-        },
-        {
-            resolve: `gatsby-plugin-static-folders`,
-            options: {
-                folders: [
-                    `./src`,
-                ]
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`,
+              macros: {
+                "\\title": "\\textbf{\\Large #1}"
+              }
             }
-        },
-        {
-            resolve: 'gatsby-plugin-netlify', options: {
-                headers: {
-                    '/4.11/4_11.pdf': [
-                        'X-Frame-Options: ALLOW-FROM https://ml.jiwai.win',
-                    ]
-                }
-            }
+          },
+          {
+            resolve: `gatsby-remark-mermaid`,
+            options: /** @type {import("gatsby-remark-mermaid").Options} */ ({
+              mermaidConfig: {
+                theme: "neutral",
+                themeCSS: ".node rect { fill: #fff; }"
+              }
+            })
+          }
+        ]
+      }
+    },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/jeff-tian.png` // This path is relative to the root of the site.
+      }
+    },
+    {
+      resolve: `gatsby-plugin-static-folders`,
+      options: {
+        folders: [
+          `./src`
+        ]
+      }
+    },
+    {
+      resolve: "gatsby-plugin-netlify", options: {
+        headers: {
+          "/4.11/4_11.pdf": [
+            "X-Frame-Options: ALLOW-FROM https://ml.jiwai.win",
+            "Content-Security-Policy: frame-ancestors https://ml.jiwai.win"
+          ]
         }
-    ],
+      }
+    }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,131 +1,102 @@
 import * as React from "react"
-import {graphql, Link} from "gatsby"
-import {StaticImage} from "gatsby-plugin-image"
+import { graphql, Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
 const links = [
-    {
-        text: "Tutorial",
-        url: "https://www.gatsbyjs.com/docs/tutorial",
-        description:
-            "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    },
-    {
-        text: "Examples",
-        url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-        description:
-            "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-    },
-    {
-        text: "Plugin Library",
-        url: "https://www.gatsbyjs.com/plugins",
-        description:
-            "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    },
-    {
-        text: "Build and Host",
-        url: "https://www.gatsbyjs.com/cloud",
-        description:
-            "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    },
+  {
+    text: "Tutorial",
+    url: "https://www.gatsbyjs.com/docs/tutorial",
+    description:
+      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site."
+  },
+  {
+    text: "Examples",
+    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
+    description:
+      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites."
+  },
+  {
+    text: "Plugin Library",
+    url: "https://www.gatsbyjs.com/plugins",
+    description:
+      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community."
+  },
+  {
+    text: "Build and Host",
+    url: "https://www.gatsbyjs.com/cloud",
+    description:
+      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!"
+  }
 ]
 
 const samplePageLinks = [
-    {
-        text: "Page 2",
-        url: "page-2",
-        badge: false,
-        description:
-            "A simple example of linking to another page within a Gatsby site",
-    },
-    {text: "TypeScript", url: "using-typescript"},
+  {
+    text: "Page 2",
+    url: "page-2",
+    badge: false,
+    description:
+      "A simple example of linking to another page within a Gatsby site"
+  },
+  { text: "TypeScript", url: "using-typescript" }
 ]
 
-const moreLinks = [
-    {text: "Join us on Discord", url: "https://gatsby.dev/discord"},
-    {
-        text: "Documentation",
-        url: "https://gatsbyjs.com/docs/",
-    },
-    {
-        text: "Starters",
-        url: "https://gatsbyjs.com/starters/",
-    },
-    {
-        text: "Showcase",
-        url: "https://gatsbyjs.com/showcase/",
-    },
-    {
-        text: "Contributing",
-        url: "https://www.gatsbyjs.com/contributing/",
-    },
-    {text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues"},
-]
-
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
-
-const IndexPage = ({data}) => (
-    <Layout>
-        <div className={styles.textCenter}>
-            <StaticImage
-                src="../images/example.png"
-                loading="eager"
-                width={64}
-                quality={95}
-                formats={["auto", "webp", "avif"]}
-                alt=""
-                style={{marginBottom: `var(--space-3)`}}
-            />
-            <h1>
-                Welcome to <b>Gatsby!</b>
-            </h1>
-            <h2>Hello</h2>
-            <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-            {data.allMarkdownRemark.edges.map(({node}) => (
-                <div key={node.id}>
-                    <h3>
-                        <Link to={`${node.fields.slug}`}>
-                            {node.fields.slug}
-                        </Link>
-                    </h3>
-                    <p>{node.excerpt}</p>
-                </div>
-            ))}
-            <p className={styles.intro}>
-                <b>Example pages:</b>{" "}
-                {samplePageLinks.map((link, i) => (
-                    <React.Fragment key={link.url}>
-                        <Link to={link.url}>{link.text}</Link>
-                        {i !== samplePageLinks.length - 1 && <> · </>}
-                    </React.Fragment>
-                ))}
-                <br/>
-                Edit <code>src/pages/index.js</code> to update this page.
-            </p>
+const IndexPage = ({ data }) => (
+  <Layout>
+    <div className={styles.textCenter}>
+      <StaticImage
+        src="../images/jeff-tian.png"
+        loading="eager"
+        width={64}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt=""
+        style={{ marginBottom: `var(--space-3)` }}
+      />
+      <h1>
+        《<b><a href="/MachineLearningTomMitchell.pdf" target="_blank" rel="noopener noreferer">机器学习</a></b>》课后作业
+      </h1>
+      <h2></h2>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            <Link to={`${node.fields.slug}`}>
+              {node.fields.slug}
+            </Link>
+          </h3>
+          <p>{node.excerpt}</p>
         </div>
-        <ul className={styles.list}>
-            {links.map(link => (
-                <li key={link.url} className={styles.listItem}>
-                    <a
-                        className={styles.listItemLink}
-                        href={`${link.url}${utmParameters}`}
-                    >
-                        {link.text} ↗
-                    </a>
-                    <p className={styles.listItemDescription}>{link.description}</p>
-                </li>
-            ))}
-        </ul>
-        {moreLinks.map((link, i) => (
-            <React.Fragment key={link.url}>
-                <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-                {i !== moreLinks.length - 1 && <> · </>}
-            </React.Fragment>
+      ))}
+      <p className={styles.intro}>
+        <b>Example pages:</b>{" "}
+        {samplePageLinks.map((link, i) => (
+          <React.Fragment key={link.url}>
+            <Link to={link.url}>{link.text}</Link>
+            {i !== samplePageLinks.length - 1 && <> · </>}
+          </React.Fragment>
         ))}
-    </Layout>
+        <br />
+        Edit <code>src/pages/index.js</code> to update this page.
+      </p>
+    </div>
+    <ul className={styles.list}>
+      {links.map(link => (
+        <li key={link.url} className={styles.listItem}>
+          <a
+            className={styles.listItemLink}
+            href={`${link.url}${utmParameters}`}
+          >
+            {link.text} ↗
+          </a>
+          <p className={styles.listItemDescription}>{link.description}</p>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 )
 
 export const query = graphql`
@@ -160,6 +131,6 @@ export const query = graphql`
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home"/>
+export const Head = () => <Seo title="Home" />
 
 export default IndexPage
